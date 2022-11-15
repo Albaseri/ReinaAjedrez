fe4d898e6bebe31c63bf147814d257c10ea617c5
Alba Sedeño <Alba Sedeo@LAPTOP-PVUVFJ3D>
2022-11-15 12:05:05 +0100

    Creo los métodos get and set para cada atributo. Los métodos sets comprueban la validez de los parámetros.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -3,4 +3,27 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Reina {
 	private Color color;
 	private Posicion posicion;
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("Has introducido un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("Has introducido una posición nula.");
+		}
+		this.posicion = posicion;
+	}
+
 }
