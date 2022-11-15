d931ff64425d1aa049b2a35721f2dc7e95a5c4e0
Alba Sedeño <Alba Sedeo@LAPTOP-PVUVFJ3D>
2022-11-15 10:44:29 +0100

    Creo métodos getters and setters de fila y columna. Añado una excepción del tipo IllegalArgumentException.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,6 +1,42 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-private int fila;
-private char columna;
+
+	private static int MIN_F = 1;
+	private static int MAX_F = 8;
+	private static char MIN_C = 'a';
+	private static char MAX_C = 'h';
+	private int fila;
+	private char columna;
+
+//getters and setters
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila < MIN_F) {
+			throw new IllegalArgumentException("La posición introducida es menor que el mínimo permitido");
+		} else if (fila > MAX_F) {
+
+			throw new IllegalArgumentException("La posición introducida es mayor que el mínimo permitido");
+		}
+		this.fila = fila;
+
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna < MIN_C) {
+			throw new IllegalArgumentException("La posición introducida es menor que el mínimo permitido");
+		} else if (columna > MAX_C) {
+
+			throw new IllegalArgumentException("La posición introducida es mayor que el mínimo permitido");
+		}
+		this.columna = columna;
+	}
+
 }
